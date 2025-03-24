5dc760cd44bf975800841f1eb43e06f2c9e6d454
Jessica <158276794+Jessicagarg258@users.noreply.github.com>
2025-03-24 17:17:47 +0530

    Delete src/boolean-to-text.pipe.spec.ts

--- src/boolean-to-text.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BooleanToTextPipe } from './boolean-to-text.pipe';
-
-describe('BooleanToTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BooleanToTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
